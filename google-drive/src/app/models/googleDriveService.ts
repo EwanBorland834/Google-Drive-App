31f44a2ec14ab3e9c5d422fa348547acf5f7712f
Ewan <natirheonul@gmail.com>
2021-10-13 13:28:32 +0100

    removing an unused function

--- google-drive/src/app/models/googleDriveService.ts
+++ google-drive/src/app/models/googleDriveService.ts
@@ -39,19 +39,4 @@ export class GoogleDriveService {
     getComments(fileName: string) {
         return of(this.comments.filter(c => c.filename === fileName));
     }
-
-    // private handleError(error: HttpErrorResponse) {
-    //     if (error.status === 0) {
-    //         // A client-side or network error occurred. Handle it accordingly.
-    //         console.error('An error occurred:', error.error);
-    //     } else {
-    //         // The backend returned an unsuccessful response code.
-    //         // The response body may contain clues as to what went wrong.
-    //         console.error(
-    //         `Backend returned code ${error.status}, body was: `, error.error);
-    //     }
-    //     // Return an observable with a user-facing error message.
-    //     return throwError(
-    //         'Something bad happened; please try again later.');
-    // }
 }
